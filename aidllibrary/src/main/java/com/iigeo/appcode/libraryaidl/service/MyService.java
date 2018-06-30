2e23a1ac2749d78166cec0cf11ce140e373bf294
win7yang <win7yang@gmail.com>
2018-06-30 22:57:16 +0800

    add Binder,CustomView and Handler

--- aidllibrary/src/main/java/com/iigeo/appcode/libraryaidl/service/MyService.java
+++ aidllibrary/src/main/java/com/iigeo/appcode/libraryaidl/service/MyService.java
@@ -1,5 +1,4 @@
 package com.iigeo.appcode.libraryaidl.service;
-
 import android.app.Service;
 import android.content.Intent;
 import android.os.Binder;
@@ -13,9 +12,11 @@ public class MyService extends Service {
 
     private static final String TAG="服务端进程";
     public MyService() {
+
     }
     //继承Binder抽象类，自定义实现Binder方法
     private Binder mBinder=new IMyAidlInterface.Stub() {
+
         @Override
         public void basicTypes(int anInt, long aLong, boolean aBoolean, float aFloat, double aDouble, String aString) throws RemoteException {
 
